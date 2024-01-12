bd14470d549bbfeb3ea7b7768fde435f9cf748ae
Benas Palivonas <palivonas.benas@gmail.com>
2024-01-12 10:41:04 +0200

    feat: add styling

--- apps/wix-app/src/app/app.component.ts
+++ apps/wix-app/src/app/app.component.ts
@@ -12,17 +12,17 @@ export class AppComponent {
     name: 'Root',
     children: [
       {
-        name: 'Category 1',
+        name: 'Node 1',
         children: [
-          { name: 'Subcategory 1.1', children: [] },
-          { name: 'Subcategory 1.2', children: [] },
+          { name: 'Node 1.1', children: [] },
+          { name: 'Node 1.2', children: [] },
         ],
       },
       {
-        name: 'Category 2',
+        name: 'Node 2',
         children: [
-          { name: 'Subcategory 2.1', children: [] },
-          { name: 'Subcategory 2.2', children: [] },
+          { name: 'Node 2.1', children: [] },
+          { name: 'Node 2.2', children: [] },
         ],
       },
     ],
